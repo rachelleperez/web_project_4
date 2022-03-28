3f500dca95fecd8ef4de7751a037f64fa0174179
Rachelle Perez <55248765+rachelleperez@users.noreply.github.com>
2022-03-28 07:14:02 -0400

    reformats adding initial cards

---
+++ scripts/data.js
@@ -0,0 +1,39 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+  // ,{
+  //   name: "Grand Canyon",
+  //   link: "https://images.unsplash.com/photo-1491258524513-1e5b31b77452?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+  // }
+  // ,{
+  //   name: "Big Sur",
+  //   link: "https://images.unsplash.com/photo-1577940800897-c082cd6790f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+  // }
+  // ,{
+  //   name: "Crater Lake",
+  //   link: "https://images.unsplash.com/photo-1517639735409-5d4a41f000ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+  // }
+
+];
--- scripts/index.js
+++ scripts/index.js
@@ -91,74 +91,42 @@ function updateBio() {
 // Card Variables
 let elementsSection = document.querySelector('.elements');
 
-const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg"
-  }
-  // ,{
-  //   name: "Grand Canyon",
-  //   link: "https://images.unsplash.com/photo-1491258524513-1e5b31b77452?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-  // }
-  // ,{
-  //   name: "Big Sur",
-  //   link: "https://images.unsplash.com/photo-1577940800897-c082cd6790f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-  // }
-  // ,{
-  //   name: "Crater Lake",
-  //   link: "https://images.unsplash.com/photo-1517639735409-5d4a41f000ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-  // }
+//Card Template
+const cardTemplate = document.querySelector('#card-template');
 
-];
+// Render Card
+function createCard(data) {
+  // create new card
+  const card = cardTemplate.content.cloneNode(true).querySelector('.elements__element');
+  //console.log(card.querySelector('.elements__image').style);
+  card.querySelector('.elements__image').style.backgroundImage = `url('${data.link}')`
+  card.querySelector('.elements__name').textContent = data.name
+  console.log(card);
+  //add event listeners
 
-//empty string to collect html tags for all cards
-let initialCardsString = `
-`;
-
-//for each card, add html tags to initialCardsString
-initialCards.forEach(fillInitialCardsString);
-
-function fillInitialCardsString (cardDict) {
-  let initialCardsStringAdd = `
-    <article class = "elements__element">
-    <img class = "elements__image" data-modal="#modal_image" src = "${cardDict["link"]}" alt = "Picture of Place">
-    <button type="button" class = "elements_delete-button"></button>
-    <div class = "elements__container">
-    <h2 class = "elements__name">${cardDict["name"]}</h2>
-    <button type="button" aria-label = "Like Button" class = "elements__like-symbol"></button>
-    </div>
-    </article>
-  `;
-  initialCardsString += initialCardsStringAdd
+  // return the created card
+  return card
 }
 
-// setDefaults will run upon loading
-window.onload = setDefaults;
-
-// function that will include ANY function that must run upon navigating to page
-function setDefaults() {
-  elementsSection.innerHTML = initialCardsString; //textContent does not work
+function renderCard (data, selector) {
+  // get a card
+  const card = createCard(data);
+  // render a card
+  document.querySelector(selector).append(card);
+  
 }
+
+initialCards.forEach(place => {
+  renderCard(place, '.elements');
+})
+
+// // setDefaults will run upon loading
+// window.onload = setDefaults;
+
+// // function that will include ANY function that must run upon navigating to page
+// function setDefaults() {
+//   
+// }
 
 // ------------------------ ADD NEW PLACE ---------------------------
 
