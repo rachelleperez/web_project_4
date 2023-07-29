ae2d523ebbdf5b8200e0a5244e8183728442a042
Rachelle Perez <rachelleaperez@gmail.com>
2023-07-29 01:13:08 -0400

    renames page dir to pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './src/page/index.js',
+  entry: './src/pages/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
