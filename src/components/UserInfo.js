6a79c23c8833265a3f40d32ced04d45c804cf899
Rachelle Perez <rachelleaperez@gmail.com>
2023-07-30 15:10:21 -0400

    UserInfo done

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,1 +1,15 @@
-export default class UserInfo {}
+export default class UserInfo {
+    constructor({ name, bio }) { // OR className instead of selector
+        this._name = document.getElementById(`${name}`);
+        this._bio = document.getElementById(`${bio}`);
+    }
+
+    getUserInfo() {
+        const user = {
+            name: this._name.textContent,
+            bio: this._bio.textContent,
+        }
+        return user
+    }
+
+}
