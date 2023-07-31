0f78673883f7de1b9a0c4e3d61663c6538a4d198
Rachelle Perez <rachelleaperez@gmail.com>
2023-07-31 16:22:09 -0400

    nicer layout in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,97 +1,107 @@
 import "./index.css";
 
 // Import all the classes
-import initialCards from '../utils/data';
-import FormValidator from '../components/FormValidator';
-import Card from '../components/Card';
+import initialCards from "../utils/data";
+import FormValidator from "../components/FormValidator";
+import Card from "../components/Card";
 
-import Section from '../components/Section';
-import PopupWithImage from '../components/PopupWithImage';
-import PopupWithForm from '../components/PopupWithForm';
-import UserInfo from '../components/UserInfo';
+import Section from "../components/Section";
+import PopupWithImage from "../components/PopupWithImage";
+import PopupWithForm from "../components/PopupWithForm";
+import UserInfo from "../components/UserInfo";
 
-import {selectors, overlay} from '../utils/constants';
-
+import { selectors, overlay } from "../utils/constants";
 
 // ------------------------ DEFAULT CARDS ---------------------------
 
-const cardSection = new Section(
-    {
-        renderer: (data) => renderCard(data, true),
-        selector: selectors.cardSection,
-    }
-);
+const cardSection = new Section({
+  renderer: (data) => renderCard(data, true),
+  selector: selectors.cardSection,
+});
 
 function renderCard(data, shouldAppend) {
-    const card = new Card({data, handleImageClick: (imgData) => {
+  const card = new Card(
+    {
+      data,
+      handleImageClick: (imgData) => {
         cardPreviewPopup.open(imgData);
-    }}, selectors.cardTemplate).createCard();;
-    cardSection.addItem(card, shouldAppend);
+      },
+    },
+    selectors.cardTemplate,
+  ).createCard();
+  cardSection.addItem(card, shouldAppend);
 }
 
 cardSection.renderItems(initialCards);
 
 // ------------------------ CARD PREVIEW ---------------------------
 
-const cardPreviewPopup = new PopupWithImage (selectors.imagePreview);
+const cardPreviewPopup = new PopupWithImage(selectors.imagePreview);
 
 // ------------------------ NEW CARD ---------------------------
 
-const addCardPopup = new PopupWithForm ({popupSelector:'modal_add', 
-    handleFormSubmit: (data) => renderCard(data, false)
+const addCardPopup = new PopupWithForm({
+  popupSelector: "modal_add",
+  handleFormSubmit: (data) => renderCard(data, false),
 });
 
-const addCardButton = document.querySelector('.profile__add-button');
+const addCardButton = document.querySelector(".profile__add-button");
 
-addCardButton.addEventListener('click', () => {
-    addFormValidator.clearValidationErrors();
-    addCardPopup.open();
-})
+addCardButton.addEventListener("click", () => {
+  addFormValidator.clearValidationErrors();
+  addCardPopup.open();
+});
 
 // ------------------------ PROFILE INFO STORAGE ---------------------------
 
-const currentUserProfile = new UserInfo ({name:'display_profile_name', bio: 'display_profile_bio'})
+const currentUserProfile = new UserInfo({
+  name: "display_profile_name",
+  bio: "display_profile_bio",
+});
 
 // ------------------------ PROFILE INFO MANAGEMENT ---------------------------
 
-const editProfilePopup = new PopupWithForm ({popupSelector:'modal_profile', 
-    handleFormSubmit: (data) => { 
-        currentUserProfile.setUserInfo(data.input_profile_name, data.input_profile_bio);
-    }
+const editProfilePopup = new PopupWithForm({
+  popupSelector: "modal_profile",
+  handleFormSubmit: (data) => {
+    currentUserProfile.setUserInfo(
+      data.input_profile_name,
+      data.input_profile_bio,
+    );
+  },
 });
 
-const editProfileButton = document.querySelector('.profile__edit-button');
+const editProfileButton = document.querySelector(".profile__edit-button");
 
-editProfileButton.addEventListener('click', () => {
-    editFormValidator.clearValidationErrors();
-    prefillProfileForm();
-    editProfilePopup.open();
-})
+editProfileButton.addEventListener("click", () => {
+  editFormValidator.clearValidationErrors();
+  prefillProfileForm();
+  editProfilePopup.open();
+});
 
 // To prefill Edit Profile Form
 
-const inputProfileName = document.getElementById('input_profile_name')
-const inputBio = document.getElementById('input_profile_bio');
+const inputProfileName = document.getElementById("input_profile_name");
+const inputBio = document.getElementById("input_profile_bio");
 
 function prefillProfileForm() {
-    const currentUser = currentUserProfile.getUserInfo();
-    inputProfileName.value = currentUser.name;
-    inputBio.value = currentUser.bio;
+  const currentUser = currentUserProfile.getUserInfo();
+  inputProfileName.value = currentUser.name;
+  inputBio.value = currentUser.bio;
 }
 
 // ------------------------ FORM VALIDATION ---------------------------
 
-
-const addForm = document.getElementById('form_add_place');
-const profileForm = document.getElementById('form_edit_profile');
+const addForm = document.getElementById("form_add_place");
+const profileForm = document.getElementById("form_edit_profile");
 
 const formValidationConfig = {
-    inputSelector: ".form__input",
-    submitButtonSelector: ".form__submit", //# = id, . is clss
-    inactiveButtonClass: "form__submit_disabled", // classes
-    inputErrorClass: "form__input_type_error",
-    errorClass: "form__error_visible"
-  };
+  inputSelector: ".form__input",
+  submitButtonSelector: ".form__submit", //# = id, . is clss
+  inactiveButtonClass: "form__submit_disabled", // classes
+  inputErrorClass: "form__input_type_error",
+  errorClass: "form__error_visible",
+};
 
 const addFormValidator = new FormValidator(formValidationConfig, addForm);
 addFormValidator.enableValidation();
