c7abb206ff23bba05c34843c2eff044adf3f3442
Rachelle Perez <rachelleaperez@gmail.com>
2023-07-31 16:23:32 -0400

    nicer layout in FormValidator.js

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -1,98 +1,98 @@
 class FormValidator {
-    
   //settings = covid, formElement = no need to loop through all forms. Say upfront the form needed
   constructor(config, formElement) {
     this._inputSelector = config.inputSelector;
     this._submitButtonSelector = config.submitButtonSelector;
     this._inactiveButtonClass = config.inactiveButtonClass;
-    this._inputErrorClass = config.inputErrorClass,
-    this._errorClass = config.errorClass;
+    (this._inputErrorClass = config.inputErrorClass),
+      (this._errorClass = config.errorClass);
 
     this._formElement = formElement;
 
-    this._inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector)); 
-    this._submitButton = this._formElement.querySelector(this._submitButtonSelector);
+    this._inputList = Array.from(
+      this._formElement.querySelectorAll(this._inputSelector),
+    );
+    this._submitButton = this._formElement.querySelector(
+      this._submitButtonSelector,
+    );
   }
 
-    //done
+  //done
   _showError(errorEl, inputEl) {
     inputEl.classList.add(this._inputErrorClass);
     errorEl.textContent = inputEl.validationMessage;
     errorEl.classList.add(this._errorClass);
   }
 
-    //done
+  //done
   _hideError(errorEl, inputEl) {
     inputEl.classList.remove(this._inputErrorClass);
     errorEl.classList.remove(this._errorClass);
-    errorEl.textContent = '';
+    errorEl.textContent = "";
   }
 
   _isInvalidInput(inputEl) {
     return !inputEl.validity.valid;
   }
 
-  _checkInputValidity(inputEl) { //settings, formEl removed
+  _checkInputValidity(inputEl) {
+    //settings, formEl removed
     const errorEl = this._formElement.querySelector(`#${inputEl.id}-error`); // added "-error" to error versions
     if (this._isInvalidInput(inputEl)) {
       // hide the error messages and style
       this._showError(errorEl, inputEl);
-    }
-    else {
+    } else {
       // show error messages and style
       this._hideError(errorEl, inputEl);
     }
   }
 
-  toggleButton() { //removed settings
+  toggleButton() {
+    //removed settings
 
     if (this._hasInvalidInputs()) {
       // disable the button
-      this._submitButton.disabled=true;
+      this._submitButton.disabled = true;
       this._submitButton.classList.add(this._inactiveButtonClass);
-    }
-    else {
+    } else {
       this._submitButton.disabled = false;
       this._submitButton.classList.remove(this._inactiveButtonClass);
-    } 
+    }
   }
 
   // returns true if some invalid input
-  _hasInvalidInputs () {
-    return this._inputList.some(inputEl => this._isInvalidInput(inputEl));
+  _hasInvalidInputs() {
+    return this._inputList.some((inputEl) => this._isInvalidInput(inputEl));
   }
 
   _setEventListeners() {
     // loop through the inputs and add validation
     this._inputList.forEach((inputEl) => {
-      inputEl.addEventListener('input', () => {
+      inputEl.addEventListener("input", () => {
         //check the input
         this._checkInputValidity(inputEl); //settings, formEl removed
         // update the button (if input is valid, enable. if not, disabled)
         this.toggleButton();
-      })
-    })
-
+      });
+    });
   }
 
   //only public function, applies to a specific form now.
   enableValidation() {
-    this._formElement.addEventListener('submit', (evt) => {
+    this._formElement.addEventListener("submit", (evt) => {
       evt.preventDefault();
-    })
+    });
     //setup validation
     this._setEventListeners(); //settings (config) to be passed to form via constructor
   }
 
   // no errors upon opening a form.
   clearValidationErrors() {
-    this._inputList.forEach(inputEl => {
+    this._inputList.forEach((inputEl) => {
       const errorEl = this._formElement.querySelector(`#${inputEl.id}-error`);
       this._hideError(errorEl, inputEl);
     });
   }
-
-
 }
 
 //export for index.js
